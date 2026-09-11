ecfdea6463c960a0be9f5b4ecb5bda8717353150
FELIPE DE OLIVEIRA COSTA <43482351805@info.local>
2026-09-11 09:27:46 -0300

    :iphone:Adcionando reponsividade
    :sparkles:Incrementando arquivo do JavaScript

---
+++ home/home.js
@@ -0,0 +1,14 @@
+
+const menu = document.getElementById("menu"); //pegando o elemento pelo ID
+//querySelector pega de forma generica #ID  .class
+const navMenu = document.querySelector(".navegação")
+
+menu.addEventListener("click", function(){
+
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo"
+    }
+    else{
+        navMenu.className = "navegacao"
+    }
+})
