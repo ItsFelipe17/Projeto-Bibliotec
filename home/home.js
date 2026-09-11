3b14618f2f13f795972d979dc8f4c2c530b23d99
FELIPE DE OLIVEIRA COSTA <43482351805@info.local>
2026-09-11 09:41:35 -0300

    :bug:Arrumando JS

--- home/home.js
+++ home/home.js
@@ -1,14 +1,14 @@
 
 const menu = document.getElementById("menu"); //pegando o elemento pelo ID
 //querySelector pega de forma generica #ID  .class
-const navMenu = document.querySelector(".navegação")
+const navMenu = document.querySelector(".navegacao")
 
 menu.addEventListener("click", function(){
 
     if(navMenu.className == "navegacao") {
-        navMenu.className = "navegacao ativo"
+        navMenu.className = "navegacao ativo";
     }
     else{
-        navMenu.className = "navegacao"
+        navMenu.className = "navegacao";
     }
 })
